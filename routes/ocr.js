51eef8d3b9d2ee4fd0e709588ff431122f5b7f21
Galuh Alifani <galuh.adika@gmail.com>
2021-08-04 14:54:59 +0700

    timeout

--- routes/ocr.js
+++ routes/ocr.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const OCRController = require("../controllers/ocrController");
+var timeout = require('connect-timeout')
 // const extendTimeoutMiddleware = require("../middlewares/extendTimeout");
 // const imageKit = require("../middlewares/imageKit");
 
@@ -8,13 +9,18 @@ const multer = require("multer");
 const upload = multer();
 
 // router.post('/', OCRController.postOcr)
-
+router.use(timeout('60s'))
 router.post(
   "/",
   upload.single("receiptImage"),
+  haltOnTimedout,
   // imageKit,
   // extendTimeoutMiddleware,
   OCRController.postOcr
 );
+
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
 
 module.exports = router;
